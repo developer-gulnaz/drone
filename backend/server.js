cca044c27fcc3d2fb681c5c771f2890de459334f
Gulnaz Sheikh <gulnazsheikh.dev@gmail.com>
2025-09-30 16:05:07 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ app.use(session({
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24, // 1 day
     sameSite: "lax",
-    secure: false
-    // secure: process.env.NODE_ENV === "production" // true in production
+    // secure: false
+    secure: process.env.NODE_ENV === "production" // true in production
   }
 }));
 
@@ -57,6 +57,6 @@ app.get("/api/debug-session", (req, res) => {
 });
 
 // Use Railway port or fallback to 5000
-const PORT = 5000;
-// const PORT = process.env.PORT || 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
